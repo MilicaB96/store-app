bfdb90e6211a02ac09ff60f689b40e03087b804f
Milica <magiflowerchild@gmial.com>
2022-03-20 14:32:02 +0100

    added customer listing and delete customer button

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Customer() {
-  return <></>;
+function Customer({ id, name, surname, deleteCallback }) {
+  return (
+    <div className='container  row'>
+      <h4 className='d-inline-block col-5'>
+        {name} {surname}
+      </h4>
+      <button
+        className='col-3 btn btn-outline-dark btn-sm'
+        type='button'
+        onClick={() => {
+          deleteCallback(id);
+        }}
+      >
+        Delete Customer
+      </button>
+    </div>
+  );
 }
 
 export default Customer;
--- src/pages/AppCustomers.js
+++ src/pages/AppCustomers.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Customer from "../components/Customer";
+import CustomerService from "../services/CustomerService";
 
 function AppCustomers() {
-  return <></>;
+  // declare customers variable
+  const [customers, setCustomers] = useState(CustomerService.getAll());
+  //delete customer function
+  const handleDelete = (id) => {
+    const isDeleted = CustomerService.delete(id);
+    if (isDeleted) {
+      setCustomers(customers.filter((customer) => customer.id !== id));
+    }
+  };
+  return (
+    <div className='p-3'>
+      <ul>
+        {customers.map((customer) => (
+          <li key={customer.id} className='text-start'>
+            <Customer {...customer} deleteCallback={handleDelete} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default AppCustomers;
